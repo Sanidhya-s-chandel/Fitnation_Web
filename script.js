989c74075fcfafb45fa831e365c596d83dfe79a5
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-09-07 01:50:41 +0530

    Almost completed

--- script.js
+++ script.js
@@ -16,3 +16,66 @@ menu.addEventListener("click",()=>{
         flag = 0
     }
 })
+
+Products = [{
+    name: "Jacket",
+    image : "./jacket-removebg-preview.png"
+},{
+    name: "Watch",
+    image: "./watch-removebg-preview.png"
+},{
+    name: "Bag",
+    image: "./bag-removebg-preview.png"
+},{
+    name: "Bottel",
+    image: "./bottel-removebg-preview.png" 
+},{
+    name: "Protiens",
+    image: "./protiens-removebg-preview.png"
+},{
+    name: "Shoes",
+    image: "./shoes-removebg-preview.png"
+}];
+
+var slider = document.querySelector(".slider")
+container = "";
+
+Products.forEach((e)=> {
+    container += `<div class="p1 h-full max-sm:h-[50vh] max-sm:w-[300px] w-[500px] text-center">
+                    <img src="${e.image}" alt="err" class="h-full w-full object-contain object-center border-b-4 border-[#96FC04]">
+                    <h1 class="m-4 text-2xl font-semibold">${e.name}</h1>
+                </div>`
+});
+
+slider.innerHTML = container
+
+var tl = gsap.timeline()
+
+tl.from(".nav h1",{
+    y : 50,
+    opacity: 0,
+    duration: 1.5,
+    delay: 0.5,
+    stagger: 0.5
+})
+
+tl.from(".nav-responsive h1",{
+    y : 50,
+    opacity: 0,
+    duration: 1.5,
+    delay: 0.1,
+    stagger: 0.5
+})
+
+gsap.to(".products .slider",{
+    transform: "translateX(-240%)",
+    scrollTrigger:{
+        trigger: ".products",
+        scroller: "Body",
+        start: "top 250",
+        end: "top -40%",
+        scrub: 5,
+        pin: true,
+        duration:2,
+    }
+})
